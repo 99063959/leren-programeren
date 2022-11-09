fa87cd01a3c6f06c38cf1b34b064efd16915f46d
owen <99063959@mydavinci.nl>
2022-11-09 12:07:49 +0100

    kaas

---
+++ module 3/kaasflow.py
@@ -0,0 +1,16 @@
+kaasKleur = input("is de kaas geel? ")
+
+if kaasKleur == "ja":
+    kaasGaten = input("zitten er gaten in? ")
+
+if kaasGaten == "ja":
+    kaasPrijs = input("is de kaas belachelijk duur? ")
+
+else:
+    kaasSchimmel = input("heeft de kaas blauweschimmel? ")
+
+if kaasSchimmel == "ja":
+    kaasKorst = input("heeft de kaas korst? ")
+
+if kaasKorst == "ja":
+    print("")
